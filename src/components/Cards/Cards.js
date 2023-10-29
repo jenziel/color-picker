a81390612f98228f87be46556a1c38247ff626ec
Jen Ziel <jenziel400@gmail.com>
2023-10-29 16:38:46 -0400

    Add functionality to colors so that when clicked, they change the color of the text above.

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Cards.css'
+import {useDispatch }from "react-redux";
+import {changeColor} from '../../features/theme'
 
 function Cards({color, id}) {
+    const [color1, setColor1] = useState("");
+    const dispatch = useDispatch();
     const darkcolors = ["darkred", "indigo","black", "rebeccapurple", "darkmagenta", "purple", "darkslateblue", "darkgreen", "blue", "mediumblue", "darkblue", "navy", "midnightblue", "maroon", "darkslategray" ]
+
+    const handleButtonClick = (selectedColor) => {
+        setColor1(selectedColor); 
+        dispatch(changeColor(selectedColor)); 
+    };
     if (darkcolors.includes(color) ) {
-        return (<button className='card' id={id} style={{background: color, color: "lightgray"}}>{color}</button>);
+        return (<button  onClick={() => handleButtonClick(color)} className='card' id={id} style={{background: color, color: "lightgray"}}>{color}</button>);
     }
   return (
-      <button className='card' id={id} style={{background: color}}>{color}</button>
+      <button 
+      onClick={() => handleButtonClick(color)}
+        className='card' id={id} style={{background: color}}>{color}</button>
   )
 }
 
 export default Cards;
+
+// onChange={(event) => {setColor(event.target.value)}
+// onClick={() => {
+//     dispatch(changeColor(color))
+// }}
