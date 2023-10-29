7665d9d06cf61b547cef3f3e6f2e7e15ab863713
Jen Ziel <jenziel400@gmail.com>
2023-10-29 16:22:36 -0400

    Add better responsive design

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,22 @@
 import "./App.css";
 import Profile from "../Profile/Profile";
-import  PreviewText from '../PreviewText/PreviewText'
+import PreviewText from "../PreviewText/PreviewText";
 // import Login from "../Login/Login";
 import Header from "../Header/Header";
 import ChangeColor from "../ChangeColor/ChangeColor";
 import ColorChart from "../ColorChart/ColorChart";
-// import {colorChart} from '../../images/css_colors.png' 
+// import {colorChart} from '../../images/css_colors.png'
 function App() {
   return (
     <main>
       <Header />
       <div className='App'>
-            {/* <Login /> */}
-            <div className="inputs-container">
-            <PreviewText />
+        <div className='inputs-container'>
+          <PreviewText />
           <ChangeColor />
-
-            </div>
-            <Profile />
-            <ColorChart /> 
-     
-
+        </div>
+        <Profile />
+        <ColorChart />
       </div>
     </main>
   );
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,10 +4,10 @@ import './Cards.css'
 function Cards({color, id}) {
     const darkcolors = ["darkred", "indigo","black", "rebeccapurple", "darkmagenta", "purple", "darkslateblue", "darkgreen", "blue", "mediumblue", "darkblue", "navy", "midnightblue", "maroon", "darkslategray" ]
     if (darkcolors.includes(color) ) {
-        return (<div className='card' id={id} style={{background: color, color: "lightgray"}}>{color}</div>);
+        return (<button className='card' id={id} style={{background: color, color: "lightgray"}}>{color}</button>);
     }
   return (
-      <div className='card' id={id} style={{background: color}}>{color}</div>
+      <button className='card' id={id} style={{background: color}}>{color}</button>
   )
 }
 
